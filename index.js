80b6c5d2d42a536bbec6e062c20eaabfc79f72de
BallThana <thana@depthfirst.co.th>
2020-06-03 21:19:48 +0700

    initial javascript

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("Hello World")
+var id = 3
+var name = "Thana"
+var ok = true
+var student = { id:1 }
+var student2 = JSON.parse(JSON.stringify(student))
+student2.id =2
+console.log(student.id)
