8d2823bff0f04585d0055e2dca830ec7cca8885f
BallThana <thana@depthfirst.co.th>
2020-06-03 21:25:55 +0700

    stringify

--- ex02-stringify.js
+++ ex02-stringify.js
@@ -5,7 +5,6 @@ var student1 = {
 var student2 = student1
 student2.id = 2
 console.log(student1.id, student2.id);
-
 var student3 = JSON.parse(JSON.stringify(student1))
 student3.id = 3
 console.log(student1.id, student3.id)
